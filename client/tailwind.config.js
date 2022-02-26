552200744256a97743490e4003ea1cfd9071ce78
Tommy La <tommyl.dmd@gmail.com>
2022-02-26 09:39:50 -0800

    landing page test & tailwindcss installed

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'mainbackground': "url('../../../client/src/images/background.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
