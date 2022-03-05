d373a1c4079a433ff960ccffbe6123eb6d90e17b
unknown <robbyhill20@yahoo.com>
2022-03-05 15:02:12 -0800

    profile summary

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -37,78 +37,63 @@ const Signup = () => {
     }
   };
   
-    return (
-      <main>  <h1 className="mb-4 text-2xl font-bold text-center text-gray-700">
-      Create an account
-    </h1>
-    {data ? (
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+          <div className="card-body">
+            {data ? (
               <p>
                 Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
+                <Link to="">back to the homepage.</Link>
               </p>
             ) : (
-      <form  onSubmit={handleFormSubmit}>
-    <div className="bg-slate-600 bg-fixed bg-cover bg-center flex items-center min-h-screen">
-      <div className="flex-1 h-full max-w-lg mx-auto opacity-95 bg-white rounded-lg shadow-xl">
-        <div className="flex flex-col md:flex-row">
-          <div className="md:flex-1 md:h-full md:max-w-lg md:mx-auto items-center justify-center p-6 sm:p-12 md:w-1/2">
-            <div className="w-full">
-              
-              <div>
-                <label className="block font-bold text-sm">
-                  Username
-                </label>
-                <input type="text"
-                  className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600 mb-4"
-                  id="first-name" 
-                  name= "username"
-                  placeholder="username"
-                  value={formState.username}
-                  onChange={handleChange}/>
-              </div>
-              <div>
-                <label className="block font-bold mt-4 text-sm">
-                  Email
-                </label>
+              <form onSubmit={handleFormSubmit}>
                 <input
-                  className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-                  id="email-signup"
-                  name= "email"
-                  type= 'text'
-                  placeholder="Email Address" 
+                  className="form-input"
+                  placeholder="Your username"
+                  name="username"
+                  type="text"
+                  value={formState.username}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
                   value={formState.email}
-                  onChange={handleChange}/>
-              </div>
-              <div className="mt-4">
-                <label className="block font-bold text-sm">
-                  Password
-                </label>
-                <input type="password"
-                  className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-                  id="password-signup"
-                  name= "password" 
-                  placeholder="Password" 
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
                   value={formState.password}
-                  onChange={handleChange}/>
-              </div>
-              <button
-                className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-black transition-colors duration-150 bg-gray-200 border border-transparent rounded-lg hover:bg-blue-500 hover:text-white focus:outline-none focus:shadow-outline-blue"
-                href="#" type= 'submit'>
-                Sign up
-              </button>
-              {error && (
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
               <div className="my-3 p-3 bg-danger text-white">
                 {error.message}
               </div>
             )}
-            </div>
           </div>
         </div>
       </div>
-    </div>
-  </form>
-            )}
-  </main>
-    );
-}
+    </main>
+  );
+};
+
 export default Signup
