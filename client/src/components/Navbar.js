7d318222f3d65017c05966ade1467392e5079321
unknown <robbyhill20@yahoo.com>
2022-03-06 17:50:17 -0800

    working on remove portfolio

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -32,12 +32,12 @@ const Navbar=() =>{
             </>
           ) : (
             <>
-              {/* <Link className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" to="/login">
+              <Link className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" to="/login">
                 Login
               </Link>
               <Link className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" to="/signup">
                 Signup
-              </Link> */}
+              </Link>
             </>
           )}
         </div>
