241b2f092a151d0eb385920e21bac643faea5954
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-03-07 20:55:15 -0800

    testing deploy

--- server/server.js
+++ server/server.js
@@ -23,11 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
