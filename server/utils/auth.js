ab933845b27010ca533e27ab341f57b520314f81
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-03-08 00:58:12 -0800

    Debugged heroku login error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-const secret = process.env.JWT_SECRET;
+const secret = 'mysecretshhh';
 const expiration = '2h';
 
 module.exports = {
