637559147356413d50204ec0e6a15e1b7c628c71
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-03-07 16:50:03 -0800

    Heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1/rate-my-portfolio-app',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+require('dotenv').config()
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
